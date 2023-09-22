be761c97570373ed1eac8e3207a334fa8ac83e9d
nicu <gg.nicu>
2023-09-22 10:09:02 -0700

    task015

---
+++ HomeWork002Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 7)
+{
+    Console.WriteLine($"нет дня недели с таким номером");
+}
+else
+{
+    bool DayWeek(int num, int num1)
+    {
+       return num > num1;
+    }
+
+    bool result = DayWeek(number, 5);
+    Console.Write(result ? $"{number}-> да" : $"{number} -> нет");
+}
+
