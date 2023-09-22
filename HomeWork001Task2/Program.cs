19e4287c9724927fe532641c046b1aa930c746e1
nicu <gg.nicu>
2023-09-21 23:14:05 -0700

    task002

--- HomeWork001Task2/Program.cs
+++ HomeWork001Task2/Program.cs
@@ -1,7 +1,7 @@
 ﻿
-Console.WriteLine("Введите 1 число");
+Console.WriteLine("Введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите 2 число");
+Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
 {
