243a124ff45ac3cac335878c28cc4ee42b239c3f
nicu <gg.nicu>
2023-09-21 22:04:17 -0700

    1

--- task005/Program.cs
+++ task005/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+bool IsOneSquareOfTheOther(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+
+Console.WriteLine($"ввидите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"ввидите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsOneSquareOfTheOther(number1, number2);
+Console.WriteLine(result ? "да" : "нет");
