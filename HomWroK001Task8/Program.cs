1eec01e0377ddce0355ce477e530639ad6b7d858
nicu <gg.nicu>
2023-09-21 23:38:29 -0700

    task008

---
+++ HomWroK001Task8/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int count = 1;
+    
+    if (number < 2)
+        {
+            Console.WriteLine($"нет четных чисел");
+        }
+
+       Console.Write($"{number} -> ");
+
+    while (count <= number)
+    {
+
+        if (count % 2 == 0)
+        {
+            Console.Write($"{count}, ");
+            count++;
+        }
+        else
+        {
+            count++;
+        }
+
+    }
+}
+
