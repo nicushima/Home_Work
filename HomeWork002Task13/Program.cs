a0573e90b9385ac2ae3ffb8f81f63c5e0abe406a
nicu <gg.nicu>
2023-09-22 08:49:17 -0700

    task013

---
+++ HomeWork002Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = number;
+
+if (num < 100)
+{
+   Console.WriteLine($"третьей цифры нет ");
+}
+else
+{
+
+   while (num > 999)
+   {
+      num = num / 10;
+   }
+
+   num = num % 10 % 10;
+   Console.WriteLine($"{number} -> {num}");
+}
+
