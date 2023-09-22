16d4fde0b01e9565731032cce12d6fea51c1acb9
nicu <gg.nicu>
2023-09-21 22:55:18 -0700

    задача 2

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"a = {number1}; b = {number2}; max = {number1}");
+}
+else if (number1 < number2)
+{
+   Console.WriteLine($"a = {number1}; b = {number2}; max = {number2}");
+}
+else 
+{
+    Console.WriteLine($"a = {number1}; b = {number2}; a = b");
+}
