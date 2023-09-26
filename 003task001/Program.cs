fdfe705af2468a7cfad636dcc11b345622154c2d
nicu <gg.nicu>
2023-09-25 20:55:26 -0700

    1

---
+++ 003task001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+string GetCoordinatesRange(int num)
+{
+    if (num == 1) return "x > 0, y > 0";
+    if (num == 2) return "x < 0, y > 0";
+    if (num == 3) return "x < 0, y < 0";
+    if (num == 4) return "x > 0, y < 0";
+    return null;
+}
+
+Console.WriteLine("ввидите номер четверти");
+int quater = Convert.ToInt32(Console.ReadLine());
+
+
+string getCoordinatesRange = GetCoordinatesRange(quater);
+
+string result = getCoordinatesRange != null ? $"заданному номеру четверти соответствует диапозон {getCoordinatesRange}" 
+                                       : $"введены не коректные данные";
+Console.WriteLine(result);
