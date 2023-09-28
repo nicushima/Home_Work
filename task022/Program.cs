e3c13c65e7399c57408ec84a4e257716d9482553
nicu <gg.nicu>
2023-09-28 16:53:02 -0700

    1

---
+++ task022/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+void TableSquare(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($" {count,3} {count * count,5}");
+        count++;
+    }
+}
+
+Console.WriteLine("ввидите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableSquare(number);
+
+
