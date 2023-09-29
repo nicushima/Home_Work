9e36d511cecd13ff410750240af31aec6330c240
nicu <gg.nicu>
2023-09-28 22:00:00 -0700

    task026

---
+++ tassk026/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int CountDigits(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("ввидите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int countDigits = CountDigits(number);
+Console.WriteLine($"{number} -> {countDigits}");
