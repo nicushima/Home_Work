8120becaa216170a0043c5f7261928891aedf198
nicu <gg.nicu>
2023-10-05 17:37:07 +0300

    HW

---
+++ HOMEWORK/HomeWork004Task29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу,
+// которая задаёт массив из 8 
+// элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int[] array = CreateArrayRndInt(8, 0, 99);
+
+Console.Write("[");
+PrintArray(array);
+Console.WriteLine("]");
