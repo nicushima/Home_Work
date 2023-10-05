d1c970956838f2091bc1115325778ffa5007d31d
nicu <gg.nicu>
2023-10-05 17:41:31 +0300

    HW

---
+++ HOMEWORK/HomeWork004Task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает
+// на вход два числа (A и B) и
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int NaturalDegree(int numA, int numB)
+{
+    int num = numA;
+    for (int i = 0; i < numB; i++)
+    {
+        num = num * numA;
+    }
+    return num;
+}
+
+
+Console.WriteLine("ввидите первое целое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ввидите второе целое число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int naturalDegree = NaturalDegree(number1, number2);
+Console.WriteLine($"{number1}, {number2} -> {naturalDegree}");
