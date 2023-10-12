dd07c39f1d705d56ef93e70d06154c7a23f379dd
nicu <gg.nicu>
2023-10-12 20:52:26 +0300

    1

--- task055/Program.cs
+++ task055/Program.cs
@@ -1,7 +1,8 @@
-﻿// Задача 53:
+﻿// Задача 55:
 // Задайте двумерный массив. Напишите программу,
-// которая поменяет местами первую и последнюю строку
-// массива.
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
 
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
@@ -34,20 +35,27 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
+// 1 2 3 
+// 2 3 1
+// 3 2 1
+
+
+
 void ReplaceFirstLastRows(int[,] matrix)
 {
-    
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    for (int i = 0; i < matrix.GetLength(0) - 1; i++)
     {
-        int temp = matrix[0,j];
-        matrix[0,j] = matrix[matrix.GetLength(0) - 1,j];
-        matrix[matrix.GetLength(0) - 1,j] = temp;
+        for (int j = i+1; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i,j];
+            matrix[i,j] = matrix[j,i];
+            matrix[j,i] = temp;
+        }
     }
 }
 
-int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+int[,] array2d = CreateMatrixRndInt(4, 4, -100, 100);
 PrintMatrix(array2d);
 ReplaceFirstLastRows(array2d);
 Console.WriteLine();
 PrintMatrix(array2d);
-
