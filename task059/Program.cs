dcf552e1fabb37f88fbd0f8e0cf03fc04c689866
nicu <gg.nicu>
2023-10-16 20:25:26 +0300

    1

---
+++ task059/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 59:
+// Задайте двумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец, на
+// пересечении которых расположен наименьший элемент
+// массива.
+
+int[] GetIndexsMinElement(int[,] matrix)
+{
+    
+}
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    //                        0      1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        //Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        //Console.WriteLine("  |");
+        Console.WriteLine();
+    }
+}
+
+
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
---
+++ task063/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+Console.WriteLine("ввидите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 1)
+{
+    Console.WriteLine("некоректный ввод");
+    return;
+}
+
+void PrintNaturalNumbers(int num)
+{
+    if (num == 0) return;
+    PrintNaturalNumbers(num - 1);
+}
