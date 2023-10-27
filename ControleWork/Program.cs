d544ead0c2d2daa2f83a586a446b6b628dd26bd9
nicu <gg.nicu>
2023-10-27 19:56:23 +0300

    1

--- ControleWork/Program.cs
+++ ControleWork/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в 
+// промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 1)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+PrintNaturalNumbers(number);
+
+void PrintNaturalNumbers(int num)
+{
+    if(num == 0) return;
+    PrintNaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
